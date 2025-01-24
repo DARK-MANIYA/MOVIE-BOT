a21b6e521a3f61e1c4529235600599757d8b21e4
DARK-MANIYA <Gihindunew2k24@gmail.com>
2025-01-24 15:37:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID: 'DARK-SHUTER-MD=RYpxwLiY#IqvPszJyKlnMzeU8Vn6JUagCudbbTCUUdw4G61bo1HY',
+SESSION_ID: 'DARK-SHUTER-MD=HdsSgDxQ#NqlJrixXPAsFl3JTSxLisQnbTbAUr5T-W-LXXH1yNUI',
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 CHAT_BOT: process.env.CHAT_BOT === undefined ? 'true' : process.env.CHAT_BOT, 
-DELETE_MSG_SENDTO: '94787318729',
+DELETE_MSG_SENDTO: '94743218422',
 SUDO: '',
-GITHUB_AUTH_TOKEN: 'PpE2mjFlSn9MCg6JoskzWTiKNpePnn4FVYY6',
-GITHUB_USER_NAME: 'sadassssr',
+GITHUB_AUTH_TOKEN: '',
+GITHUB_USER_NAME: '',
 
 };
 //GITHUB_AUTH_TOKEN: 'ouvnI0SDsmfWA1ilVxSZ0vJGYC5VX54U0e10',
